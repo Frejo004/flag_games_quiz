9150949cdd0a426544fc663e4a6b8d83bfeeaf23
Frejus DASSI <frejohope@gmail.com>
2025-02-17 10:07:24 +0100

    initilisation du projet game flag

---
+++ android/app/src/main/kotlin/com/example/flag_games_quiz/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flag_games_quiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/score.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScoreGame extends StatefulWidget {
+  const ScoreGame({super.key});
+
+  @override
+  State<ScoreGame> createState() => _ScoreGameState();
+}
+
+class _ScoreGameState extends State<ScoreGame> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
