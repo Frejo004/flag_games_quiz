9e99f2cb5100b8cafdca126ae52a63ce96fe3d56
Frejus DASSI <frejohope@gmail.com>
2025-02-17 10:32:13 +0100

    mise en place de la page d'acceuil et une animation vers la page de jeu

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,3 +1,5 @@
+import 'package:animations/animations.dart';
+import 'package:flag_games_quiz/screen/game.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,6 +7,44 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              '🎮🧭🌍🌏🌎',
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            Text(
+              'Flags Quiz',
+              style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                  fontWeight: FontWeight.w700,
+                  color: Theme.of(context).colorScheme.onPrimaryFixedVariant),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                 Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) {
+                      return const GamePage();
+                    },
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) {
+                      return FadeThroughTransition(
+                        animation: animation,
+                        secondaryAnimation: secondaryAnimation,
+                        child: child, 
+                      ); 
+                    },
+                  ),);
+              },
+            child: Text('New Game'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
