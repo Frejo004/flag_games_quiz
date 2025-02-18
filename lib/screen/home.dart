20a6b387ad76845a707b03f604d2f850fc577e50
Frejus DASSI <frejohope@gmail.com>
2025-02-18 10:12:29 +0100

    Refonte de l'écran d'accueil avec ajout d'une image de fond et mise à jour du bouton de démarrage de jeu

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,6 +1,7 @@
 import 'package:animations/animations.dart';
 import 'package:flag_games_quiz/screen/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -8,44 +9,55 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              '🎮🧭🌍🌏🌎',
-              style: Theme.of(context).textTheme.displayLarge,
+      body: Stack(
+        fit: StackFit.expand, 
+        children: [
+          Image.asset(
+            '/countries.png',
+            fit: BoxFit.cover,
+          ),
+          // Your existing widgets
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  '🎮',
+                  style: Theme.of(context).textTheme.displayLarge,
+                  selectionColor: Colors.white,
+                ),
+                Text(
+                  'Flags Quiz',
+                  style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                        fontWeight: FontWeight.w700,
+                        color: const Color.fromARGB(255, 255, 255, 255),
+                      ),
+                ),
+                SizedBox(height: 35,),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) {
+                          return const GamePage();
+                        },
+                        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                          return FadeThroughTransition(
+                            animation: animation,
+                            secondaryAnimation: secondaryAnimation,
+                            child: child,
+                          ); 
+                        },
+                      ),
+                    );
+                  },
+                  child: Text('Jouer une partie'),
+                ),
+              ],
             ),
-            Text(
-              'Flags Quiz',
-              style: Theme.of(context).textTheme.displayLarge!.copyWith(
-                  fontWeight: FontWeight.w700,
-                  color: Theme.of(context).colorScheme.onPrimaryFixedVariant),
-            ),
-            SizedBox(height: 15,),
-            
-            ElevatedButton(
-              onPressed: () {
-                 Navigator.push(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (context, animation, secondaryAnimation) {
-                      return const GamePage();
-                    },
-                    transitionsBuilder:
-                        (context, animation, secondaryAnimation, child) {
-                      return FadeThroughTransition(
-                        animation: animation,
-                        secondaryAnimation: secondaryAnimation,
-                        child: child, 
-                      ); 
-                    },
-                  ),);
-              },
-            child: Text('New Game'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
