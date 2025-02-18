3f5e5dbecdaccf0f1c3796da994546dfd81d419e
Frejus DASSI <frejohope@gmail.com>
2025-02-18 10:12:03 +0100

    Suppression de la route vers l'écran de score dans la configuration des routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flag_games_quiz/screen/game.dart';
 import 'package:flag_games_quiz/screen/home.dart';
-import 'package:flag_games_quiz/screen/score.dart';
 
 
 class Routes {
   final Map<String, WidgetBuilder> _routes = 
     {
       '/game': (context) => const GamePage(),
-      '/score': (context) => const ScoreGame(),
       '/': (context) => const Home(),
     };
 
